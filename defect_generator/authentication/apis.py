04890cf062e2fac80451b6e69215d9bfb150ab62
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2023-07-01 18:46:40 +0330

    chore: update signup api swagger

--- defect_generator/authentication/apis.py
+++ defect_generator/authentication/apis.py
@@ -1,12 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from drf_spectacular.utils import extend_schema
 
 from defect_generator.authentication.services import AuthenticationService
 from defect_generator.authentication.serializers import UserSignupInputSerializer
 
 
 class SignupApi(APIView):
+
+    @extend_schema(request=UserSignupInputSerializer)
     def post(self, request):
         serializer = UserSignupInputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -19,5 +22,5 @@ class SignupApi(APIView):
         except Exception as ex:
             return Response(f"Error {ex}", status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_200_OK)
 
