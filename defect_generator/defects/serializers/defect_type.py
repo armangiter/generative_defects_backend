94ff0b53025f798d23313dbf36a69cb110fe2c97
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2023-07-27 20:19:40 +0330

    chore: add icon image to defect_type apis response

--- defect_generator/defects/serializers/defect_type.py
+++ defect_generator/defects/serializers/defect_type.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from defect_generator.defects.models import DefectType
+from defect_generator.defects.utils import get_real_url
 
 
 
@@ -15,9 +16,16 @@ class DefectTypeInputSerializer(serializers.Serializer):
 
 
 class DefectTypeOutputSerializer(serializers.ModelSerializer):
+    icon_image = serializers.SerializerMethodField()    
+
     class Meta:
         model = DefectType
-        fields = ("id", "name", "command", "defect_model_id")
+        fields = ("id", "name", "command", "defect_model_id", "icon_image")
+
+    def get_icon_image(self, obj: DefectType):
+        if bool(obj.icon_image) is True:
+            return get_real_url(obj.icon_image.url)
+        return None
 
 
 class DefectTypeDetailInputSerializer(serializers.Serializer):
@@ -26,6 +34,13 @@ class DefectTypeDetailInputSerializer(serializers.Serializer):
 
 
 class DefectTypeDetailOutputSerializer(serializers.ModelSerializer):
+    icon_image = serializers.SerializerMethodField() 
+
     class Meta:
         model = DefectType
-        fields = ("id", "name", "command")
+        fields = ("id", "name", "command", "icon_image")
+
+    def get_icon_image(self, obj: DefectType):
+        if bool(obj.icon_image) is True:
+            return get_real_url(obj.icon_image.url)
+        return None
